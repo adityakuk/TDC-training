c7280ea68a6f25915bd30f68dc35f183fb432a1d
adityakuk <singhaditya2869@gmail.com>
2024-02-02 18:16:18 +0530

    add footer logic

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+const footer = [
+    { items: ["Contact Us"] }, { items: [] }, { items: [] }
+];
+
+debugger
+const innerHtml = footer.map(({ items }) => (`
+    <div class="col-md-4">
+    ${items.map((item) => (`<h5>${item}</h5>`))}
+    </div>
+`)).join("")
+
+document.querySelector("#footer").innerHTML = innerHtml
